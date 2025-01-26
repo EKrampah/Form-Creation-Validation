90c82d130385102aa4fd4f55807deb08857cb250
Eric Krampah <ekrampah253@gmail.com>
2025-01-26 22:41:20 +0000

    modified

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(){
         feedbackDiv.textContent = "Registration successful";
         feedbackDiv.style.color = "#28a745";
       } else {
-        feedbackDiv.textContent = messages.join("<br>");
+        feedbackDiv.innerHTML = messages.join("<br>");
         feedbackDiv.style.color = "#dc3545";
       }
     })

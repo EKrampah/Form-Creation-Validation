443789c0f0263133d7b21e0d3548291085a06d72
Eric Krampah <ekrampah253@gmail.com>
2025-01-26 22:31:05 +0000

    fetch-data js updated

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    async function fetchData () {
+    
+    const apiUrl = "https://jsonplaceholder.typicode.com/users"
+    const dataContainer = document.getElementById("api-data")
+
+    try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    dataContainer.innerHTML = " ";
+    const userList = document.createElement("li");
+    
+    users.forEach(user => {
+        const listItem = document.createElement("li")
+        listItem.textContent = user.name
+        userList.appendChild(listItem);
+    });
+
+    dataContainer.appendChild(userList);
+    }
+
+    catch (errror) {
+        dataContainer.innerText = " ";
+        dataContainer.textContent = "failed to load user data";
+    }
+}
+    fetchData();
+})
